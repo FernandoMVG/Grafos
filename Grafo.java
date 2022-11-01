622549c485bef0979c69462681c707b9c2884852
Fernando Mateo Valencia Gómez <98895066+FernandoMVG@users.noreply.github.com>
2022-11-01 09:26:06 -0500

    Arreglo del bucle de impresión de matriz

---
+++ Grafo.java
@@ -0,0 +1,258 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package graphmaker;
+
+import static graphmaker.Lienzo.getNodos;
+import static graphmaker.Maker.obl;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ASUS TUF GAMING F15
+ */
+public class Grafo {
+
+    ArrayList<vertices> vertices = null;
+    ArrayList<aristas> conexiones = null;
+    int nvertices = 0;
+    String Recorrido[][];
+    int Distancia[][];
+    ArrayList<aristas> ruta = null;
+    ArrayList<vertices> caminoMinimo = new ArrayList<>();
+    int inPos;
+    int finPos;
+    int INF = 999;
+
+    public Grafo() {
+        this.conexiones = new ArrayList<>();
+        this.vertices = new ArrayList<>();
+    }
+
+    //Contar cuantos vertices hay
+    public void aumentar() {
+        this.nvertices++;
+    }
+
+    // ***** Eliminar vertice *****
+    public void delVertice(vertices v) {
+
+        for (aristas a : v.conexiones) {
+            Maker.graf.conexiones.remove(a);
+            obl.getEnlaces().remove(a);
+        }
+        this.vertices.remove(v);
+
+        Maker.estado.setText("Nodo " + v.getName() + " eliminado");
+        obl.repaint();
+    }
+
+    // ***** Eliminar vertice *****
+    public void delArista(aristas a) {
+        for (aristas act : this.conexiones) {
+            if (act == a) {
+                a.inicio.conexiones.remove(act);
+                a.fin.conexiones.remove(act);
+                this.conexiones.remove(act);
+            }
+        }
+        Maker.estado.setText("Arista " + a.inicio.getName() + ", " + a.fin.getName() + " eliminada");
+        obl.repaint();
+
+    }
+// ***** Buscar vertice por coordenadas *****
+
+    public vertices buscar(int x, int y) {
+        Rectangle figura = new Rectangle(x - 20, y - 20, 40, 40);
+        for (vertices v : getNodos()) {
+            if (new Rectangle(v.getX() - v.d / 2, v.getY() - v.d / 2, v.d, v.d).intersects(figura)) {
+                return v;
+            }
+        }
+        return null;
+    }
+
+    // ***** Buscar vertice por nombre *****
+    public vertices buscar(String S) {
+        for (vertices v : this.vertices) {
+            if (v.getName().equals(S)) {
+                return v;
+            }
+        }
+        return null;
+    }
+
+    // ***** verificar si ya hay un vertice con ese nombre *****
+    public boolean existe(String S) {
+        for (vertices v : this.vertices) {
+            if (v.getName().equals(S)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // ***** Buscar arista por inicio y fin *****
+    public aristas buscarArist(String inicio, String fin) {
+        for (aristas a : this.conexiones) {
+            if (a.inicio.getName().equals(inicio) || a.fin.getName().equals(inicio)) {
+                if (a.inicio.getName().equals(fin) || a.fin.getName().equals(fin)) {
+                    return a;
+                }
+            }
+        }
+        return null;
+    }
+
+    public aristas buscarArist(vertices in, vertices fin) {
+        for (aristas a : this.conexiones) {
+            if (a.inicio == in || a.fin == in) {
+                if (a.inicio == fin || a.fin == fin) {
+                    return a;
+                }
+            }
+        }
+        return null;
+    }
+
+    public void MatrizDeCostos() {
+        Distancia = new int[vertices.size()][vertices.size()];
+        boolean ady = false;
+        int pos = -1;
+        int INF = 999;
+        for (int i = 0; i < vertices.size(); i++) {
+            System.out.println(vertices.size());
+            for (int j = 0; j < vertices.size(); j++) {
+                ady = false;
+
+                for (int k = 0; k < vertices.get(i).ady.size(); k++) {
+                    if (vertices.get(i).ady.get(k).equals(vertices.get(j))) {
+                        ady = true;
+                        pos = k;
+                    }
+
+                }
+                if (ady == true) {
+                    for (int z = 0; z < conexiones.size(); z++) {
+                        if ((conexiones.get(z).inicio.equals(vertices.get(i)) && conexiones.get(z).fin.equals(vertices.get(j)))
+                                || (conexiones.get(z).fin.equals(vertices.get(i)) && conexiones.get(z).inicio.equals(vertices.get(j)))) {
+                            Distancia[i][j] = conexiones.get(z).costo;
+                        }
+                    }
+                } else {
+                    Distancia[i][j] = INF;
+                }
+                if (i == j) {
+                    Distancia[i][j] = 0;
+                }
+            }
+
+        }
+        Recorrido = new String[vertices.size()][vertices.size()];
+        for (int i = 0; i < vertices.size(); i++) {
+            for (int j = 0; j < vertices.size(); j++) {
+                Recorrido[i][j] = vertices.get(j).getName();
+            }
+        }
+//        //Ayuda para ver que todo ande bien
+//        System.out.println("----------");
+//        System.out.println("Tamaño de vertices.size(): " + vertices.size());
+//        System.out.println("# de conexiones: " + conexiones.size());
+//        System.out.println("Distancia entre 1 y 1 vertice: " + Distancia[1][1]);
+//        System.out.println("Distancia entre 0 y 1 vertice: " + Distancia[0][1]);
+//        System.out.println("Distancia entre 0 y 2 vertice: " + Distancia[0][2]);
+
+    }
+
+    public void FloydWarshall() {
+        for (int k = 0; k < vertices.size(); k++) {
+            for (int i = 0; i < vertices.size(); i++) {
+                if(i != k){
+                    for (int j = 0; j < vertices.size(); j++) {
+                        if (j != k){
+                            if (Distancia[k][j] != INF && Distancia[i][k] != INF){
+                                if (Distancia[i][j] == INF){
+                                    Distancia[i][j] = Distancia[k][j] + Distancia[i][k];
+                                    //Recorrido[i][j] = Recorrido[i][k];
+                                    Recorrido[i][j] = vertices.get(k).getName();
+                                } else {
+                                    if (Distancia[i][j] > Distancia[i][k] + Distancia[k][j]) {
+                                        Distancia[i][j] = Distancia[i][k] + Distancia[k][j];
+                                        Recorrido[i][j] = vertices.get(k).getName();
+                                    }
+                                }
+                            }
+                        }   
+                    }
+                }               
+            }           
+        }
+    }
+    
+
+    public String CaminitoMinimo(vertices inicio, vertices fin) {
+        caminoMinimo.add(inicio);
+        for (int k = 0; k < vertices.size(); k++) {
+            if (vertices.get(k).equals(inicio)) {
+                inPos = k;
+            }
+            if (vertices.get(k).equals(fin)) {
+                finPos = k;
+            }
+        }
+        int CostoAsociado = Distancia[inPos][finPos];
+        String Ruta = vertices.get(inPos).getName();
+        boolean finalizado = false;
+        while (finalizado == false) {
+            if (Recorrido[inPos][finPos].equals(inicio.getName())
+                    || Recorrido[inPos][finPos].equals(fin.getName())) {
+                Ruta = Ruta + "-->" + fin.getName();
+                fin.paso = true;
+                caminoMinimo.add(fin);
+                finalizado = true;
+            } else {
+                Ruta = Ruta + "-->" + Recorrido[inPos][finPos];
+                vertices actual = buscar(Recorrido[inPos][finPos]);
+                actual.paso = true;
+                caminoMinimo.add(actual);
+
+                String tmp = Recorrido[inPos][finPos];
+                for (int i = 0; i < vertices.size(); i++) {
+                    if (vertices.get(i).getName().equalsIgnoreCase(tmp)) {
+                        inPos = i;
+                    }
+                }
+            }
+        }
+        Ruta = Ruta + " ------ Costo de la ruta: " + CostoAsociado;
+        if (CostoAsociado >= INF) {
+            Ruta = "No existe ruta de " + inicio.getName() + " hasta " + fin.getName();
+        }
+        return Ruta;
+    }
+
+    public void printMatrix(int[][] Matrix) {
+        Maker.mostrar.append("\n\t");
+        for (int j = 0; j < vertices.size(); j++) {
+            Maker.mostrar.append(vertices.get(j).getName() + "\t");
+        }
+        Maker.mostrar.append("\n");
+        for (int j = 0; j < (nvertices * 9 - 4); j++) {
+            Maker.mostrar.append("-");
+        }
+        Maker.mostrar.append("\n");
+        for (int i = 0; i < vertices.size(); i++) {
+            Maker.mostrar.append(vertices.get(i).getName() + " |\t");
+            for (int j = 0; j < vertices.size(); j++) {
+                Maker.mostrar.append("" + Matrix[i][j]);
+                Maker.mostrar.append("\t");
+            }
+            Maker.mostrar.append("\n");
+        }
+        Maker.mostrar.append("\n");
+    }
+
+}
---
+++ Lienzo.java
@@ -0,0 +1,308 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package graphmaker;
+
+import static graphmaker.Maker.graf;
+import static graphmaker.Maker.obl;
+import static graphmaker.crear.name;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author ASUS TUF GAMING F15
+ */
+public class Lienzo extends javax.swing.JPanel implements MouseListener {
+
+    ArrayList<vertices> Nodos = null;
+    ArrayList<aristas> Enlaces = null;
+    public static int x = 0;
+    public static int y = 0;
+    public static Point p1, p2, P1, P2, P3, P4;
+    int n = 0;
+    ArrayList<aristas> temp = new ArrayList<>();
+    Image imagen;
+    public static vertices v1,v2;
+
+    public Lienzo() {
+        this.Nodos = new ArrayList<>();
+        this.Enlaces = new ArrayList<>();
+        addMouseListener(this);
+    }
+
+    public static ArrayList<vertices> getNodos() {
+        return Maker.graf.vertices;
+    }
+
+    public static ArrayList<aristas> getEnlaces() {
+        return Maker.graf.conexiones;
+    }
+
+    public static vertices BuscarVertice(int x, int y) {
+        for (vertices vertice : getNodos()) {
+            if (vertice.x == x && vertice.y == y) {
+                return vertice;
+            }
+        }
+        return null;
+    }
+
+    public void resetFloyd() {
+        for (vertices v : graf.caminoMinimo) {
+            v.paso = false;
+        }
+        for (aristas a : obl.temp) {
+            a.color = Color.BLACK;
+        }
+
+        graf.caminoMinimo= new ArrayList<>();
+        obl.temp=new ArrayList<>();
+    }
+
+    private boolean encima(Point p) {
+
+        Rectangle figura = new Rectangle(p.x - 20, p.y - 20, 40, 40);
+        for (vertices v : getNodos()) {
+            if (new Rectangle(v.getX() - v.d / 2, v.getY() - v.d / 2, v.d, v.d).intersects(figura)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        imagen = new ImageIcon(getClass().getResource("mapa.png")).getImage();
+        g.drawImage(imagen, 0, 0, getWidth(), getHeight(), this);
+        setOpaque(false);
+        super.paint(g);
+        for (aristas a : getEnlaces()) {
+            a.pintar(g);
+        }
+        for (vertices v : getNodos()) {
+            v.pintar(g);
+
+        }
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        n++;
+        System.out.println("cp: " + (n));
+        if (e.getButton() == 1) {
+            if (Maker.vertice.isSelected()) {
+                Maker.estado.setText("Clickea el espacio de trabajo para agregar un vértice.");
+                if (!encima(e.getPoint())) {
+                    x = e.getX() - 20;
+                    y = e.getY() - 20;
+
+                    switch (Maker.enumeracion.getSelectedIndex()) {
+                        case 0:
+                            Maker.graf.aumentar();
+                            int n = 1;
+                            String name = String.valueOf((char) (Maker.graf.nvertices + 64));
+                            if (graf.existe(name)) {
+                                do {
+                                    name = String.valueOf((char) (Maker.graf.nvertices + (64 + n)));
+                                    n++;
+                                } while (graf.existe(name));
+                                Maker.graf.vertices.add(new vertices(name, Maker.graf.nvertices, x, y));
+
+                            } else {
+                                Maker.graf.vertices.add(new vertices(String.valueOf((char) (Maker.graf.nvertices + 64)), Maker.graf.nvertices, x, y));
+                            }
+
+                            obl.repaint();
+                            break;
+                        case 1:
+                            Maker.graf.aumentar();
+                            String name1 = Integer.toString(Maker.graf.nvertices);
+                            int n1 = 1;
+                            if (graf.existe(name1)) {
+                                do {
+                                    name1 = Integer.toString(Maker.graf.nvertices + n1);
+                                    n1++;
+                                } while (graf.existe(name1));
+                                Maker.graf.vertices.add(new vertices(name1, Maker.graf.nvertices, x, y));
+
+                            } else {
+                                Maker.graf.vertices.add(new vertices(Integer.toString(Maker.graf.nvertices), Maker.graf.nvertices, x, y));
+                            }
+
+                            obl.repaint();
+                            break;
+                        case 2:
+                            crear v1 = new crear(x, y);
+                            v1.setVisible(true);
+                            Maker.obl.setEnabled(false);
+                            break;
+                    }
+
+                } else {
+                    JOptionPane.showMessageDialog(null, "Espacio ocupado por un nodo", "OJITO", JOptionPane.WARNING_MESSAGE);
+                }
+
+            } else if (Maker.arista.isSelected()) {
+                Maker.estado.setText("Selecciona el vertice inicial y final de la arista.");
+                for (vertices n : getNodos()) {
+                    if (new Rectangle(n.getX(), n.getY(), n.d + 10, n.d + 10).contains(e.getPoint())) {
+                        System.out.println("SI ESTÁ CONTENIDO");
+                        //System.out.println(p1.x);
+                        if (p1 == null) {
+                            p1 = new Point(n.getX(), n.getY());
+                            System.out.println("P1 " + p1.x);
+                            Maker.estado.setText("Vertice inicial seleccionado");
+                            return;
+                        } else {
+                            System.out.println("PESO P1 " + p1.x);
+                            p2 = new Point(n.getX(), n.getY());
+                            Maker.estado.setText("Vertice final seleccionado");
+                            vertices v1 = BuscarVertice(p1.x, p1.y);
+                            vertices v2 = BuscarVertice(p2.x, p2.y);
+                            boolean permitir = true;
+                            for (aristas a : v1.conexiones) {
+                                if (a.inicio == v2 || a.fin == v2) {
+                                    permitir = false;
+                                }
+                            }
+                            if (permitir) {
+                                Conectar addAris = new Conectar(p1, p2, v1, v2);
+                                addAris.setVisible(true);
+                            } else {
+                                JOptionPane.showMessageDialog(null, "Ya hay una conexión entre los nodos elegidos", "YA EXISTE", JOptionPane.WARNING_MESSAGE);
+                            }
+
+                            System.out.println(Maker.graf.conexiones.size());
+                            p1 = null;
+                            p2 = null;
+                            return;
+                        }
+                    }
+                }
+            } else if (Maker.floyd.isSelected()) {
+
+                Maker.estado.setText("Clickea el vertice inicial y final.");
+                for (vertices n : getNodos()) {
+                    if (new Rectangle(n.getX(), n.getY(), n.d + 10, n.d + 10).contains(e.getPoint())) {
+                        if (P1 == null) {
+                            P1 = new Point(n.getX(), n.getY());
+                            Maker.estado.setText("Vertice inicial seleccionado");
+                            return;
+                        } else {
+                            P2 = new Point(n.getX(), n.getY());
+                            Maker.estado.setText("Vertice final seleccionado");
+                            //Maker.graf.MatrizDistancias();
+                            vertices v1 = BuscarVertice(P1.x, P1.y);
+                            vertices v2 = BuscarVertice(P2.x, P2.y);
+
+                            System.out.println("Vertice inicial: " + v1.getName());
+                            System.out.println("Vertice final: " + v2.getName());
+                            //Maker.graf.MatrizDeCostos();
+                            //System.out.println(Maker.graf.FloydWarshall(v1, v2));
+                            v1.paso = true;
+                            //Maker.graf.FloydWarshall();
+                            Maker.estado.setText(Maker.graf.CaminitoMinimo(v1, v2));
+                            //Maker.graf.printMatrix(Maker.graf.Distancia);
+
+                            for (vertices act : Maker.graf.caminoMinimo) {
+
+                                System.out.println("LIST NODOS CAMINO: " + act.getName());
+                                for (aristas a : act.conexiones) {
+                                    System.out.println(a.inicio.paso + " : " + a.fin.paso);
+                                    if (a.inicio.paso == true && a.fin.paso == true) {
+                                        a.color = Color.yellow;
+                                        temp.add(a);
+                                        System.out.println("AMARILLO");
+                                    }
+                                }
+                            }
+                            obl.repaint();
+
+                            graf.caminoMinimo = new ArrayList<>();
+                            P1 = null;
+                            P2 = null;
+                            return;
+                        }
+                    }
+                }
+            } else if (Maker.editarV.isSelected()) {
+                if (encima(e.getPoint())) {
+                    vertices actual = Maker.graf.buscar(e.getX() - 20, e.getY() - 20);
+                    editVertice edit = new editVertice(actual);
+                    edit.setVisible(true);
+                }
+                repaint();
+            } else if (Maker.eliminarA.isSelected()) {
+                Maker.estado.setText("Clickea el vertice inicial y final.");
+                for (vertices n : getNodos()) {
+                    if (new Rectangle(n.getX(), n.getY(), n.d + 10, n.d + 10).contains(e.getPoint())) {
+                        if (P3 == null) {
+                            P3 = new Point(n.getX(), n.getY());
+                            Maker.estado.setText("Vertice inicial seleccionado");
+                            return;
+                        } else {
+                            P4 = new Point(n.getX(), n.getY());
+                            Maker.estado.setText("Vertice final seleccionado");
+                            //Maker.graf.MatrizDistancias();
+                            vertices v1 = BuscarVertice(P3.x, P3.y);
+                            vertices v2 = BuscarVertice(P4.x, P4.y);
+
+                            System.out.println("Vertice inicial: " + v1.getName());
+                            System.out.println("Vertice final: " + v2.getName());
+                            aristas aEliminar = graf.buscarArist(v1, v2);
+                            if (aEliminar != null) {
+                                obl.getEnlaces().remove(aEliminar);
+                                v1.conexiones.remove(aEliminar);
+                                v2.conexiones.remove(aEliminar);
+                                Maker.estado.setText("Conexión entre " + v1.getName() + " y " + v2.getName() + " eliminada");
+                            } else {
+
+                                Maker.estado.setText("No existe conexión entre " + v1.getName() + " y " + v2.getName());
+                            }
+
+                            obl.repaint();
+
+                            P3 = null;
+                            P4 = null;
+                            return;
+                        }
+                    }
+                }
+            }
+
+        }
+
+    }
+    public void SelecVert(){
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+}
